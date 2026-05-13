1367582deeccca8c30461917c70317d5eeb15971
Kameba <kameba26@proton.me>
2026-05-13 13:11:41 +0200

    feat: sample dashboard + ci?

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+</script>
+<h1 class="font-heading font-extrabold">Test</h1>
+<Button>Click me</Button>
