e5161709663502dc1c90f8ab59c1ce3a092cf61b
OSINT-Dev-42 <numerator0417@proton.me>
2026-07-10 22:18:52 +0200

    added .gitkeep for graphs/general folder)

--- src/graphs/make_graphs.py
+++ src/graphs/make_graphs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pathlib as path
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 PATH = path.Path(__file__).parents[2]
@@ -8,9 +9,10 @@ PATH = path.Path(__file__).parents[2]
 CSV_PATH = PATH / 'data' / 'raw' / 'firstcrawl.csv'
 TOTAL_COUNT_PNG_PATH = PATH / 'data' / 'graphs' / 'interesting_total_count'
 DIFF_PNG_PATH = PATH / 'data' / 'graphs' / 'interesting_differences'
+GENERAL_PNG_PATH = PATH / 'data' / 'graphs' / 'general'
 
 
-def find_anomalies(df):
+def find_anomalies(df, threshold):
     # get a list of restaurants in the dataframe by unique name
 
     all_unique_restaurants = df['name'].unique()
@@ -44,10 +46,10 @@ def find_anomalies(df):
 
     max_values = pd.DataFrame(list(maximum_dict.items()), columns=['name', 'max_value'])
     max_values = max_values.sort_values('max_value')
-    max_values = max_values[max_values['max_value'] > 1]
+    max_values = max_values[max_values['max_value'] > threshold]
     
     min_values = pd.DataFrame(list(minimum_dict.items()), columns=['name', 'min_value'])
-    min_values = min_values[min_values['min_value'] < -1]
+    min_values = min_values[min_values['min_value'] < -threshold]
     min_values = min_values.sort_values('min_value')
     return min_values, max_values
 
@@ -99,6 +101,41 @@ def plot_data(df, restaurants):
 
 
 
+def make_sankey_plot(df):
+    min_values, max_values = find_anomalies(df, threshold=0)
+    total_restaurants = len(df['name'].unique())
+    number_with_deletions = len(min_values)
+    number_with_additions = len(max_values) - 1
+    add_1 = len(max_values[max_values['max_value'] < 2])
+    add_2_4 = len(max_values[(max_values['max_value'] >= 2) & (max_values['max_value'] < 5)])
+    add_5_9 = len(max_values[(max_values['max_value'] >= 5) & (max_values['max_value'] < 10)])
+    add_10_14 = len(max_values[(max_values['max_value'] >= 10) & (max_values['max_value'] < 15)])
+    add_15_19 = len(max_values[(max_values['max_value'] >= 15) & (max_values['max_value'] < 20)])
+    add_over_20 = len(max_values[(max_values['max_value'] >= 20)])
+    print(number_with_additions, add_1, add_2_4, add_5_9, add_10_14, add_15_19, add_over_20) 
+    fig = go.Figure(data=[go.Sankey(
+        node = dict(
+          thickness = 5,
+          line = dict(color = "green", width = 0.1),
+          label = ["Total", "With New Reviews", "Without New Reviews", "One New Review", "2-4 New Reviews", "5-9 New Reviews", "10-14 New Reviews"],
+          color = "blue"
+        ),
+        link = dict(
+            
+          # indices correspond to labels
+          source = [0, 0, 1, 1, 1, 1], 
+          target = [1, 2, 3, 4, 5, 6],
+          value = [number_with_additions, total_restaurants - number_with_additions, add_1, add_2_4, add_5_9, add_10_14]
+      ))])
+
+    fig.show()
+    
+    
+    
+    
+    
+
+
 df = pd.read_csv(CSV_PATH)
 # data cleaning for the line with stashed changes
 df = df[df.name != '>>>>>>> Stashed changes']
@@ -113,8 +150,8 @@ for col in star_cols:
         
         
 
-min_values, max_values = find_anomalies(df)
-restaurants = pd.concat([min_values, max_values], ignore_index=True)
-restaurants = restaurants['name'].unique()
-plot_data(df, restaurants)
-
+# min_values, max_values = find_anomalies(df, threshold=1)
+# restaurants = pd.concat([min_values, max_values], ignore_index=True)
+# restaurants = restaurants['name'].unique()
+# plot_data(df, restaurants)
+make_sankey_plot(df)
