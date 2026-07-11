26e881bbc44346e73b349abb28edd95396a78cc4
kameba26 <kameba26@proton.me>
2026-07-11 19:07:26 +0200

    fix: main link

--- webapp/src/lib/components/layout/header.svelte
+++ webapp/src/lib/components/layout/header.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import icon from '$lib/assets/star-check.svg';
 </script>
 
 <nav class="mb-12 flex w-screen items-center justify-between border-b px-12 py-4">
 	<div>
-		<a class="flex items-center gap-2" href="/"
+		<a class="flex items-center gap-2" href={dev ? '/' : "/Starving"}
 			><img src={icon} alt="Starving" class="h-8 w-8" />
 			<b class="text-4xl font-black">Starving</b></a
 		>
